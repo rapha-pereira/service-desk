d4c6fe82fa19e00db525ff7f16dc27b2889a8b52
Raphael P. Gomes <87092221+rapha-pereira@users.noreply.github.com>
2025-06-29 23:40:21 +0000

    add: login endpoint and redirect to it when accessing url

--- service-desk-app/src/app/app.routes.ts
+++ service-desk-app/src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+  },
+  {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
 ];
