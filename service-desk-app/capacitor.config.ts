bbc4dd26a79f798b002439cf722c7a916f4e93b1
Raphael P. Gomes <87092221+rapha-pereira@users.noreply.github.com>
2025-06-29 23:13:19 +0000

    chore: blank ionic app

---
+++ service-desk-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'service-desk-app',
+  webDir: 'www'
+};
+
+export default config;
